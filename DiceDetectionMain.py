a418c3b5f734f00c699805c1c16f69ae6df232c9
MarceliJerzynski <marcelijerz@gmail.com>
2019-11-12 00:42:46 +0100

    Still doesn't work, but its better

--- DiceDetectionMain.py
+++ DiceDetectionMain.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import cv2
 import random
@@ -18,7 +20,7 @@ def adjust_gamma(image, gamma):
 
 def main():
     i = 0
-    thresh = 170
+    thresh = 195
     min_threshold = 10  # these values are used to filter our detector.
     max_threshold = 100  # they can be tweaked depending on the camera distance, camera angle, ...
     min_area = 20  # ... focus, brightness, etc.
@@ -37,149 +39,35 @@ def main():
         cv2.resizeWindow( image_name_original, 450, 300 )
         cv2.moveWindow( image_name_original, 0, 100 )
 
-        # params = cv2.SimpleBlobDetector_Params()  # declare filter parameters.
-        # params.filterByArea = True
-        # # params.filterByCircularity = True
-        # # params.filterByInertia = True
-        # # params.minThreshold = min_threshold
-        # # params.maxThreshold = max_threshold
-        # params.minArea = 12
-        # params.minCircularity = min_circularity
-        # params.minInertiaRatio = min_inertia_ratio
-
-        # params = cv2.SimpleBlobDetector_Params()
-        #
-        # params.filterByArea = True
-        # params.minArea = min_area
-        #
-        # params.filterByConvexity = True
-        # params.minConvexity = obj.Convexity / 200
-
-        # detector = cv2.SimpleBlobDetector_create(params)
         original_image = cv2.imread('src\\' + header + str(i+1) + '.jpg', cv2.IMREAD_COLOR)
         gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-        gray_image = 255 - gray_image
-        # keypoints = detector.detect(gray_image)
-        # print(len(keypoints))
-        # endpoints_image = cv2.drawKeypoints(gray_image, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
+        blurred_image = cv2.GaussianBlur( gray_image, (15, 15), 0 )
+        thresh_image = cv2.adaptiveThreshold( blurred_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, 11, 1)
+        kernel = np.ones((5,5), np.uint8)
+        eroded_image = cv2.erode(thresh_image, kernel, iterations = 1)
+        dilated_image = cv2.dilate( eroded_image, kernel, iterations = 3 )
+        contours, hierarchy = cv2.findContours(dilated_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        noise_removal = cv2.bilateralFilter( gray_image, 9, 75, 75 )
-
-        _, after_threshold = cv2.threshold( noise_removal, 70, 255, cv2.THRESH_OTSU )
-
-        canny_image = cv2.Canny( after_threshold, 250, 255 )
-
-
-        kernel = np.ones( (3, 3), np.uint8 )
-        # Creating the kernel for dilation
-        dilated_image = cv2.dilate( canny_image, kernel, iterations = 1 )
-
-        contours, h = cv2.findContours( dilated_image, 1, 2 )
-        contours = sorted( contours, key = cv2.contourArea, reverse = True )[:]
-
-        cv2.drawContours(original_image, contours, -1, (0, 0, 255), 9)
-        blurred_image = cv2.GaussianBlur(gray_image,(15,15), 0)
-        thresh_image = cv2.adaptiveThreshold(blurred_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
-        kernel = np.ones((3,3), np.uint8)
-        erosion_image= cv2.erode(thresh_image, kernel, iterations = 0)
-        dilate_image = cv2.dilate( erosion_image, kernel, iterations = 3 )
-        contours, hierarchy = cv2.findContours( dilate_image, 1, 2 )
-        # print(len(contours))
         for j in range(len(contours)):
-            # if (len(contours[j]) < 5 ):
-            # ellipse = cv2.fitEllipse(contours[j])
-            #     continue
-            if ( 3500 < cv2.contourArea(contours[j])  ):# and hierarchy[0][j][3] == -1):
-                cv2.drawContours(original_image, contours, j,  (0,0,25), 3)
-            #     cv2.ellipse(original_image, ellipse, (0,255,0), 3)
-
-        # erosion_image = cv2.erode( blurred_image2, kernel, iterations = 3 )
-        # dilate_image = cv2.dilate( erosion_image, kernel, iterations = 3 )
-
-        blurred_image2 = cv2.GaussianBlur( dilate_image, (5, 5), cv2.BORDER_DEFAULT )
-
-        blurred_image3 = cv2.GaussianBlur(blurred_image2, (5, 5), cv2.BORDER_DEFAULT)
-
-        blurred_image4 = cv2.GaussianBlur( blurred_image3, (5, 5), cv2.BORDER_CONSTANT )
-
-        # erosion_image2 = cv2.erode(blurred_image4, kernel, iterations = 3)
-        #
-        # dilate_image2 = cv2.dilate( erosion_image2, kernel, iterations = 4 )
-        # colored = cv2.cvtColor(blurred_image3, cv2.COLOR_GRAY2BGR)
-        # houghed = cv2.HoughCircles(colored, cv2.HOUGH_GRADIENT, 1, 120, param1 = 100, param2 = 2, minRadius = 0, maxRadius = 0)
-        # houghed = np.unit16(houghed)
-
-        # circles = cv2.HoughCircles(blurred_image4, cv2.HOUGH_GRADIENT, 100, 30, minRadius = 0, maxRadius = 10)
-        # circles = np.uint16(np.around(circles))
-        median = cv2.medianBlur(blurred_image4, 3)
-        median = 255 - median
-        median = cv2.medianBlur( median, 5 )
-        # median = cv2.erode(median, kernel, iterations = 3)
-        median = cv2.medianBlur( median, 3 )
-        median = cv2.medianBlur( median, 3 )
-        median = cv2.medianBlur( median, 3 )
-        median = cv2.medianBlur( median, 3 )
-        median = cv2.medianBlur( median, 3 )
-        median = cv2.erode(median, kernel, iterations = 2)
-
-        median = cv2.dilate( median, kernel, iterations = 2 )
-
-        # median = 255 - median
-
-        # contours, hier = cv2.findContours( median, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE )
-        # for cnt in contours:
-        #     if 4000 < cv2.contourArea( cnt ) < 5000 and True:
-        #         cv2.drawContours( original_image, [ cnt ], 0, (0, 255, 0), 2)
-
-        cv2.imshow(image_name, median)
-        # for i in circles[0, :]:
-        #     cv2.circles(blurred_image4, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        #
-        #     cv2.circles( blurred_image4, (i[ 0 ], i[ 1 ]), i[ 2 ], (0, 255, 0), 3 )
-        #
-        # cv2.imshow("Circle", original_image)
-        # lower_black = np.array([0,0,0])
-        # upper_black = np.array([140,140,140])
-        #
-        # dots = cv2.inRange(original_image, lower_black, upper_black)
-        #
-        # params = cv2.SimpleBlobDetector_Params()  # declare filter parameters.
-        # params.filterByArea = True
-        # params.filterByCircularity = True
-        # params.filterByInertia = True
-        # params.minThreshold = min_threshold
-        # params.maxThreshold = max_threshold
-        # params.minArea = 12
-        # params.minCircularity = min_circularity
-        # params.minInertiaRatio = min_inertia_ratio
-        #
-        # params = cv2.SimpleBlobDetector_Params()
-        #
-        # detector = cv2.SimpleBlobDetector_create(params)
-        # #original_image = cv2.imread( 'src\\' + header + str( i + 1 ) + '.jpg', cv2.IMREAD_COLOR )
-        # #gray_image = cv2.cvtColor( original_image, cv2.COLOR_BGR2GRAY )
-        # #gray_image = 255 - gray_image
-        # keypoints = detector.detect(dots)
-        # print(len(keypoints))
-        # endpoints_image = cv2.drawKeypoints(original_image, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
-        #
-        # noise_removal = cv2.bilateralFilter( endpoints_image, 9, 75, 75 )
-        # gray = cv2.cvtColor(noise_removal, cv2.COLOR_BGR2GRAY)
-        # _, after_threshold = cv2.threshold( gray, 70, 255, cv2.THRESH_OTSU )
-        #
-        # canny_image = cv2.Canny( after_threshold, 250, 255 )
-        #
-        #
-        # kernel = np.ones( (3, 3), np.uint8 )
-        # # Creating the kernel for dilation
-        # dilated_image = cv2.dilate( canny_image, kernel, iterations = 1 )
-        #
-        # contours, h = cv2.findContours( dilated_image, 1, 2 )
-        # contours = sorted( contours, key = cv2.contourArea, reverse = True )[:]
-        #
-        # cv2.drawContours(original_image, contours, -1, (0, 0, 255), 9)
-        # cv2.imshow( image_name, dilated_image )
-        # cv2.imshow( image_name_original, original_image)
+            area = cv2.contourArea(contours[j])
+            #if (area < 3000 and area > 500):
+            perimeter = cv2.arcLength( contours[j], True )
+            if perimeter == 0:
+                break
+            circularity = 4 * math.pi * (area / (perimeter * perimeter))
+            if (0.5 < circularity < 1.2 and 6000 > area > 1000 ):
+                print (area)
+                cv2.drawContours(original_image, contours, j, (0,0,255), 7)
+                if hierarchy[0][j][3] >= 0 and hierarchy[0][j][2] < 0:
+                    cv2.drawContours(original_image, contours, hierarchy[0][j][3], (255, 0, 0), 5)
+                #potential_dots.append(contours[j])
+        contours2, hierarchy2 = cv2.findContours(dilated_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for j in range( len( contours ) ):
+            area = cv2.contourArea( contours2[ j ] )
+            if area > 6000:
+                cv2.drawContours(gray_image, contours2, j, (255, 0, 0), 5)
+        cv2.imshow(image_name, original_image)
+        cv2.imshow(image_name_original, gray_image)
         key = cv2.waitKey( 0 )
         if key == 27: # ESCAPE
             sys.exit()
@@ -195,10 +83,14 @@ def main():
                 i = amount_of_pictures - 1
         elif key == 101:  # E
             thresh = thresh + 5
-            print('thresh = ', thresh )
+            print ('thresh = ', thresh )
         elif key == 113:  # Q
             thresh = thresh - 5
-            print('thresh = ', thresh )
+            print ('thresh = ', thresh )
+        else:
+            print ("A/D - change image ")
+            print ("-----------------------")
+            (print("Q/E - change thresh"))
 
         cv2.destroyAllWindows()
 
